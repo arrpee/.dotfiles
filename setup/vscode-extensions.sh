22b6746caedf4b3ef4fee1bedd890800c2cbaf3b
Raaj Patil <raaj.2501@gmail.com>
2022-10-29 17:11:01 -0700

    Add vscode installation script

---
+++ setup/vscode-extensions.sh
@@ -0,0 +1,14 @@
+pkglist=(
+    ms-python.python
+    VisualStudioExptTeam.vscodeintellicode
+    MS-vsliveshare.vsliveshare-pack
+    eamodio.gitlens
+    mhutchie.git-graph
+    piotrpalarz.vscode-gitignore-generator
+    alefragnani.project-manager
+    PKief.material-icon-theme
+)
+
+for i in ${pkglist[@]}; do
+  code --install-extension $i
+done
